f590f8d4377cae63c1fc633ee38cba3de0b6fe90
joncancode <jc.haines19@gmail.com>
2017-08-31 15:31:31 -0400

    worked through exercise 2 part 4(find previous) and 5(find last)

--- linked-lists.js
+++ linked-lists.js
@@ -9,7 +9,7 @@ class LinkedList {
     this.head = null;
   }
 
-// Insertion
+  // Insertion
   insert(nthPosition, value) {
     if (nthPosition < 0 || nthPosition > this.length) {
       throw new Error('nthPosition error');
@@ -17,17 +17,16 @@ class LinkedList {
     const newNode = {
       value
     };
-    if(nthPosition === 0) {
+    if (nthPosition === 0) {
       newNode.next = this.head;
       this.head = newNode;
-    }
-    else {
+    } else {
       const node = this._find(nthPosition - 1);
       newNode.next = node.next;
       node.next = newNode;
     }
     this.length++;
-  } 
+  }
   _find(nthPosition) {
     let node = this.head;
     for (let i = 0; i < nthPosition; i++) {
@@ -36,8 +35,7 @@ class LinkedList {
     return node;
   }
 
-
-// Retrieval
+  // Retrieval
   get(nthPosition) {
     if (nthPosition < 0 || nthPosition >= this.length) {
       throw new Error('Index error');
@@ -45,16 +43,14 @@ class LinkedList {
     return this._find(nthPosition).value;
   }
 
-
-// Removal
+  // Removal
   remove(nthPosition) {
-    if(nthPosition < 0 || nthPosition >= this.length) {
+    if (nthPosition < 0 || nthPosition >= this.length) {
       throw new Error('Index error');
     }
     if (nthPosition === 0) {
       this.head = this.head.next;
-    }
-    else {
+    } else {
       const node = this._find(nthPosition - 1);
       node.next = node.next.next;
     }
@@ -79,31 +75,29 @@ list.insert(3, 'four');
 // console.log('remove 3: ', list.remove(3));
 // console.log('final: ',list);
 
-
 // --- Exercise 2 (Helper Functions) ------
 
 // display
 console.log('Display -----------');
 
 // const display = (jonlist) => {
-	//   let length = jonlist.length;
-	//   for(let i = 0; i < length; i++) {
-	//     console.log('item: ', list.get(i));
-	//   }
-	// };
-	
-const display = (jonlist) => {
+//   let length = jonlist.length;
+//   for(let i = 0; i < length; i++) {
+//     console.log('item: ', list.get(i));
+//   }
+// };
 
-	// extract the head
+const display = jonlist => {
+  // extract the head
   let node = jonlist.head;
-	
-	console.log('first node is: ', node.value);
 
-	// while loop, node.next until null, console.log(node.value)
-  while(node.next !== null) {
+  console.log('first node is: ', node.value);
+
+  // while loop, node.next until null, console.log(node.value)
+  while (node.next !== null) {
     // console.log('node------>', node);
-    node = node.next; 
-    console.log('item: ',node.value);
+    node = node.next;
+    console.log('item: ', node.value);
   }
   // console.log('node value----->', node.value);
   return;
@@ -112,36 +106,46 @@ const display = (jonlist) => {
 
 // size
 console.log('Size -----------');
-const size = (jonlist) => {
-  
+const size = jonlist => {
   let counter = 1;
   let node = jonlist.head;
   console.log('counter starts at: ', counter);
-  while(node.next !== null) {
+  while (node.next !== null) {
     counter++;
-    node = node.next; 
+    node = node.next;
     console.log('counter: ', counter);
   }
   return counter;
 };
 // console.log('list is this big: ', size(list));
 
-
 // empty
 // console.log('Empty -----------');
-const isEmpty = (list) => (list.head === null) ? true : false;
+const isEmpty = list => (list.head === null ? true : false);
 // console.log(isEmpty(list));
 // console.log(isEmpty(secondList));
 
-
 // find previous
 console.log('Find Previous -----------');
+const findPrevious = (jonlist, node) => {
+  let newNode = jonlist.head;
+  while (newNode.next.value !== node) {
+    newNode = newNode.next;
+  }
+  return newNode.value;
+};
 
-
-
-
+// console.log(findPrevious(list, 'three'));
 
 // find last
+console.log('Find Last -----------');
+const findLast = jonlist => {
+  let node = jonlist.head;
 
+  while (node.next !== null) {
+    node = node.next;
+  }
+  return node;
+};
 
-
+console.log(findLast(list));
